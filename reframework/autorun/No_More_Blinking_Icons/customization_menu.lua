328a8bc4c87d1ad933cd20278804cb5c013c7d20
GreenComfyTea <klunics97@gmail.com>
2023-05-14 09:22:18 +0300

    Cache globals

--- reframework/autorun/No_More_Blinking_Icons/customization_menu.lua
+++ reframework/autorun/No_More_Blinking_Icons/customization_menu.lua
@@ -3,6 +3,38 @@ local this = {};
 local utils;
 local config;
 
+local sdk = sdk;
+local tostring = tostring;
+local pairs = pairs;
+local ipairs = ipairs;
+local tonumber = tonumber;
+local require = require;
+local pcall = pcall;
+local table = table;
+local string = string;
+local Vector3f = Vector3f;
+local d2d = d2d;
+local math = math;
+local json = json;
+local log = log;
+local fs = fs;
+local next = next;
+local type = type;
+local setmetatable = setmetatable;
+local getmetatable = getmetatable;
+local assert = assert;
+local select = select;
+local coroutine = coroutine;
+local utf8 = utf8;
+local re = re;
+local imgui = imgui;
+local draw = draw;
+local Vector2f = Vector2f;
+local reframework = reframework;
+local os = os;
+local ValueType = ValueType;
+local package = package;
+
 this.is_opened = false;
 this.status = "OK";
 
